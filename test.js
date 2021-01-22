1a2b746b01e04da58392c00d2d5edee2965ea248
mduumar112233 <mdumarfaruk187326@gmail.com>
2021-01-22 13:46:52 -0800

    javascript assignment 3

---
+++ test.js
@@ -0,0 +1,95 @@
+//////// FIRST PROBLEM 
+
+// function kilometerToMeter(meter){
+//     if (meter != 0 && meter > 0){
+//         var result = meter * 1000;
+//     }
+//     else{
+//         console.log("You must be input valid number for check this function!!!!!!!!")
+//     }
+//     return result;
+// }
+
+// var userInput = kilometerToMeter(ddf);
+// console.log(userInput)
+
+//////// SECOND PROBLEM 
+
+// function budgetCalculator(watch, phone, laptop){
+    
+//     if (watch, phone, laptop != 0){
+//         var watch = 50 * watch;
+//         var phone = 100 * phone;
+//         var laptop = 500 * laptop;
+//         var result = watch + phone + laptop;
+//     }
+//     else{
+//         console.log("You must be input valid number for check this function!!!!!!!!")
+//     }
+//     return result;
+// }
+
+// var budgetInput = budgetCalculator(0);
+// console.log(budgetInput)
+
+
+//////// Third PROBLEM 
+
+// function hotelCost(rent){
+//     var hotelRent = 100;
+//     if (rent <= 10){
+//         var totalCost = rent * 100;
+//     }
+//     else if (rent < 10 || 21 > rent){
+//         var totalCost = rent * (100-20);
+//     }
+//     else if(20 < rent){
+//         var totalCost = rent * (100-50);
+//     }
+//     else{
+//         console.log("You can't stay here without money or you must be pay for stay!!!!!!");
+//     }
+//     return totalCost;
+// }
+
+// var payMoneyForHotel = hotelCost();
+// console.log(payMoneyForHotel)
+
+
+//////// FOURTH PROBLEM 
+
+// function magaFriend(friendList){
+//     var bigName = "";
+//     for(var i = 0; i < friendList.length; i++){
+//         if(bigName.length < friendList[i].length){
+//             bigName = friendList[i];
+//         }
+//     }
+//     return bigName;
+// }
+// var allFriend = ['rofikul', 'jubaer khan', 'manon hosan', 'mosadek', 'robiul'];
+
+// var result = magaFriend(allFriend);
+// console.log(result);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
